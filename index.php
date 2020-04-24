82b3bad48bb1efcf8e55b3e66880fd79b2d8473a
Taylor Tao <taylortaozhiyuan@outlook.com>
2020-04-24 18:53:15 -0400

    5nd Commit

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
 echo "Hello World!";
+$file = fopen('index.html','r');
+
+    if($file){
+        while(!feof($file)){
+            $line = fgetc($file);
+            echo $line;
+        }
+    }
+    fclose($file);
