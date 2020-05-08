26f770d0add2aae955a597869067a32e269f408c
Zhengzhi Wang <zw573@cornell.edu>
2020-05-08 16:49:41 -0400

    t

--- index.php
+++ index.php
@@ -6,21 +6,6 @@ $serverName = "zw573sqldb.database.windows.net"; // update me
         "Uid" => "a7149007", // update me
         "PWD" => "Wzz917917" // update me
     );
-    //Establishes the connection
-    $conn = sqlsrv_connect($serverName, $connectionOptions);
-    $tsql= "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName
-         FROM [SalesLT].[ProductCategory] pc
-         JOIN [SalesLT].[Product] p
-         ON pc.productcategoryid = p.productcategoryid";
-    $getResults= sqlsrv_query($conn, $tsql);
-    echo ("Reading data from table" . PHP_EOL);
-    if ($getResults == FALSE)
-        echo (sqlsrv_errors());
-    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-     echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
-    }
-    sqlsrv_free_stmt($getResults);
-
 ?>
 
 
