30f54e0df120932400e34d522668bfdcefd09d73
Zhengzhi Wang <zw573@cornell.edu>
2020-05-08 16:48:44 -0400

    tst

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
+<?php
+
+$serverName = "zw573sqldb.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "zw573sqldb", // update me
+        "Uid" => "a7149007", // update me
+        "PWD" => "Wzz917917" // update me
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName
+         FROM [SalesLT].[ProductCategory] pc
+         JOIN [SalesLT].[Product] p
+         ON pc.productcategoryid = p.productcategoryid";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
+
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -22,8 +48,8 @@
                         }
                     ?>
             </select>
-            <input name="cow_temp" type="text" placeholder='cow_temp'/>
-            <input name="animal_activity" type="text" placeholder='animal_activity'/>
+            <!-- <input name="cow_temp" type="text" placeholder='cow_temp'/>
+            <input name="animal_activity" type="text" placeholder='animal_activity'/> -->
             <input type="submit" value="Predict" >
         </form>
 	  </div>
